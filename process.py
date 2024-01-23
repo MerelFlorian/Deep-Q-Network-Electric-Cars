5797e5242dc476bcd18793b92138ef21b9967cbc
TheGrandNobody <thegrandnobody@protonmail.com>
2024-01-23 20:40:37 +0100

    Create additional features

--- process.py
+++ process.py
@@ -4,7 +4,7 @@ import pandas as pd
 PATH = "data/train.xlsx"
 SAVE_TO = "data/features.xlsx"
 
-def compute_ma(data: dict, df: pd.DataFrame, min: int, max: int) -> dict:
+def compute_stats(data: dict, df: pd.DataFrame, min: int, max: int) -> dict:
     """ Computes the moving average of the data and adds it to a dictionary of columns.
 
     Args:
@@ -16,17 +16,24 @@ def compute_ma(data: dict, df: pd.DataFrame, min: int, max: int) -> dict:
     Returns:
         dict: The dictionary containing the moving averages.
     """
-    # Flatten the DataFrame into a single Series
-    flat_series = df.drop('date', axis=1).values.flatten()
 
     for i in range(min, max):
         # Compute the rolling average with the specified window
-        ma = pd.Series(flat_series).rolling(window=i, min_periods=1).mean()
-        ema = pd.Series(flat_series).ewm(span=i, adjust=False).mean()
-        
-        # Store the results in the dictionary
-        data[f'{i}MA'] = ma.values
-        data[f'{i}EMA'] = ema.values
+        data[f'{i}MA'] = pd.Series(df).rolling(window=i, min_periods=1).mean()
+        data[f'{i}EMA'] = pd.Series(df).ewm(span=i, adjust=False).mean()
+    
+    # Compute the expanding average
+    data['ExMA'] = pd.Series(df).expanding(min_periods=1).mean()
+    # Compute the expanding median
+    data['ExMedian'] = pd.Series(df).expanding(min_periods=1).median()
+    # Compute the expanding min
+    data['ExMin'] = pd.Series(df).expanding(min_periods=1).min()
+    # Compute the expanding max
+    data['ExMax'] = pd.Series(df).expanding(min_periods=1).max()
+    # Compute the expanding std
+    data['ExStd'] = pd.Series(df).expanding(min_periods=1).std()
+    # Compute the expanding var
+    data['ExVar'] = pd.Series(df).expanding(min_periods=1).var()
 
     return data
 
@@ -57,45 +64,34 @@ def main(path: str, save_to:str) -> None:
     df['Day'] = df['date'].dt.weekday + 1  # Monday=1, Tuesday=2, etc.
     df['Month'] = df['date'].dt.month 
 
+    date_df = df[['date', 'Day', 'Month']]
+
     # Define columns for featureset
     features = ["3MA", "4MA", "5MA", "6MA", "7MA", "8MA", "10MA", "12MA",
                 "3EMA", "4EMA", "5EMA", "6EMA", "7EMA", "8EMA", "10EMA", "12EMA",
-                "Season", "Weekend", 'Mean', 'Median', 'Min', 'Max', 'Std', 'Var']
+                "Season", "Weekend"]
     
+    # 'Mean', 'Median', 'Min', 'Max', 'Std', 'Var'
+
     # Create an empty DataFrame with these columns
     new_df = pd.DataFrame(columns=features)
 
     # Concatenate all hourly prices into a single series for rolling calculations
-    hourly_prices = df[['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 
-                        'H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 
-                        'H19', 'H20', 'H21', 'H22', 'H23', 'H24']].values.flatten()
-
-    # Convert to a Pandas Series for rolling calculations
-    hourly_prices_series = pd.Series(hourly_prices)
+    df = df.drop(columns=['date', 'Day', 'Month']).values.flatten()
 
     new_data = {
-        'Season': df['Month'].apply(season),
-        'Weekend': df['Day'].apply(weekend),
-        'Mean':  hourly_prices_series.rolling(window=len(hourly_prices_series), min_periods=1).mean()[:len(df)],
-        'Median': hourly_prices_series.rolling(window=len(hourly_prices_series), min_periods=1).median()[:len(df)],
-        'Min': hourly_prices_series.rolling(window=len(hourly_prices_series), min_periods=1).min()[:len(df)],
-        'Max': hourly_prices_series.rolling(window=len(hourly_prices_series), min_periods=1).max()[:len(df)],
-        'Std': hourly_prices_series.rolling(window=len(hourly_prices_series), min_periods=1).std()[:len(df)],
-        'Var': hourly_prices_series.rolling(window=len(hourly_prices_series), min_periods=1).var()[:len(df)],
+        'Season': date_df['Month'].apply(season),
+        'Weekend': date_df['Day'].apply(weekend)
     }
 
-    # Add the features to the DataFrame
-    for feature in features:
-        new_df[feature] = new_data[feature]
+    # Compute the moving averages
+    new_data = compute_stats(new_data, df, 3, 13)
+
+    # Assign the new_data to the new_df
+    new_df = new_df.assign(**new_data)
 
     # Drop the NaN values
     new_df = new_df.dropna()
-
-    # Compute the moving averages
-    new_data = compute_ma(new_data, df, 3, 13)
-
-    # Create a new DataFrame
-    new_df = pd.concat(new_data.values(), axis=1)
 
     # Save the new_df to a new excel file
     new_df.to_excel(save_to, index=False)
