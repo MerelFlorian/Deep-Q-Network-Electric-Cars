f018f8e5bc44fab5cf243504500d3df9413fcb9f
ilsefeenstra <ilsef@me.com>
2024-01-23 16:35:22 +0100

    season and weekend

--- process.py
+++ process.py
@@ -4,9 +4,39 @@ import pandas as pd
 PATH = "data/train.xlsx"
 SAVE_TO = "data/features.xlsx"
 
-def main(path: str) -> None:
+def season(month: int) -> int:
+    if month in [1, 2, 12]:
+        return 1
+    elif month in [3, 4, 5]:
+        return 2
+    elif month in [6, 7, 8]:
+        return 3
+    elif month in [9, 10, 11]:
+        return 4
+
+def weekend(day: int) -> int:
+    if day in [5, 6]:
+        return 1
+    else:
+        return 0
+    
+def main(path: str, save_to:str) -> None:
     # Load the dataset
     df = clean_data(path)
+
+    # Convert 'date' column to DateTime
+    df['date'] = pd.to_datetime(df['date'])
+
+    # Extract day of the week and month
+    df['Day'] = df['date'].dt.weekday + 1  # Monday=1, Tuesday=2, etc.
+    df['Month'] = df['date'].dt.month 
+
+    # Add the 'Season' column
+    df['Season'] = df['Month'].apply(season)
+
+    # Add a 'Weekend' feature
+   
+    df['Weekend'] = df['Day'].apply(weekend)
 
     # Define columns for featureset
     features = ["3MA", "4MA", "5MA", "6MA", "7MA", "8MA", "10MA", "12MA",
@@ -16,13 +46,11 @@ def main(path: str) -> None:
     # Create an empty DataFrame with these columns
     new_df = pd.DataFrame(columns=features)
 
-    # Perform your calculations or manipulations here
-    # For example, let's create dummy data based on the original df
     new_data = {
-        'Feature1': df['Column1'] * 2,  # Assuming 'Column1' exists in your original df
-        'Feature2': df['Column2'] + 5,  # Similarly, assuming 'Column2' exists
-        'Feature3': len(df['Column3'])  # And 'Column3'
+        'Season': df['Season'],
+        'Weekend': df["Weekend"]
     }
 
 if __name__ == "__main__":
+
     main(PATH, SAVE_TO)
