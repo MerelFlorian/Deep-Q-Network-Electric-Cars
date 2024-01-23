dea65208ddfccf7714f375b3b38da8031206e5a4
ilsefeenstra <ilsef@me.com>
2024-01-23 15:31:05 +0100

    new dqn train

--- DQN_train.py
+++ DQN_train.py
@@ -17,7 +17,7 @@ def objective(trial):
     activation_fn_name = trial.suggest_categorical("activation_fn", ["relu", "tanh"])
     action_size = trial.suggest_categorical("action_size", [100, 200, 500])
     state_size = 8
-    episodes = 1
+    episodes = 10
 
     activation_fn = torch.relu if activation_fn_name == "relu" else torch.tanh
 
