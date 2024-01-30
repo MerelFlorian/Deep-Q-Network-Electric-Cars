f84a52da4c80224306f0585e7f5949635396a1c9
TheGrandNobody <thegrandnobody@protonmail.com>
2024-01-30 16:37:22 +0100

    Pull

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ def clip(action: float, state: np.ndarray) -> float:
     # Clip the action value to the minimum/maximum power
     max_action = min(action, min(25,  (50 - state[0]) * 0.9)) if action > 0 else 0
     min_action = max(action, -min(25, state[0] * 0.9)) if action < 0 else 0
+    print(action, np.clip(action, min_action, max_action) / 25)
     return np.clip(action, min_action, max_action) / 25
 
 def save_best_q(best_q_table: np.ndarray, highest_reward: float, episode: int, filename: str = 'best_q_table.npy'):
