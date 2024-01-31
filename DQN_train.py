358aadb3cd0bf51e49fb3c9f394a3ee4a51c04ec
ilsefeenstra <ilsef@me.com>
2024-01-31 17:43:41 +0100

    DQN train

--- DQN_train.py
+++ DQN_train.py
@@ -92,15 +92,12 @@ def objective(trial):
     Function to optimize the hyperparameters of the DQN agent.
     """
     # Define the hyperparameter search space using the trial object
-    lr = trial.suggest_float("lr", 1e-3, 1e-2, log=True)
-    gamma = trial.suggest_float("gamma", 0.01, 0.3)
-    action_size = trial.suggest_categorical("action_size", [10, 15, 20, 25])
+    lr = trial.suggest_float("lr", 3e-4, 1e-2, log=True)
     batch_size = trial.suggest_categorical("batch_size", [1, 2, 4, 8, 16, 32, 64, 128])
+    sequence_length = trial.suggest_int("sequence_length", 1, 30)
 
-
-    # For LSTM
-    sequence_length = 1
-    
+    gamma = 0.01
+    action_size = 24
     episodes = 20
 
     # Create the environment and agent
@@ -198,77 +195,6 @@ def train_DQN_LSTM(env, agent, model, test_env, test_agent, episodes, sequence_l
     
     agent.save(model_save_path)
  
-    return avg_val_reward
-
-def train_DQN_Linear(env, agent, model, test_env, episodes, model_save_path):
-    """
-    Function to train the DQN agent.
-    """
-
-    total_train_rewards, total_val_rewards = [], []
-    
-    for episode in range(episodes):
-        state = env.reset()
-        done = False
-        states, episode_rewards = [], []
-
-        # Ensure the state has the correct shape
-        if not isinstance(state, np.ndarray) or state.shape != (len(env.state),):
-            state = np.reshape(state, (len(env.state),))  
-
-        while not done:    
-            action_index, action = agent.choose_action(state)
-            next_state, reward, done, _, _ = env.step(action)
-            episode_rewards.append(reward)
-            agent.remember(state, action, action_index, reward, next_state, done)
-
-            # Experience replay
-            if len(agent.memory) > agent.replay_size:
-                agent.replay()
-            
-            state = next_state
-            states.append(torch.from_numpy(state).float())
-
-        total_train_rewards.append(sum(episode_rewards))
-        print(f"Episode {episode + 1}: Total Reward: {sum(episode_rewards)}")
-
-    # Validate the agent
-    agent.epsilon = 0  # Set epsilon to 0 to use the learned policy without exploration
-    counter = 0
-
-    for episode in range(episodes):
-        state = test_env.reset()
-        val_episode_rewards, states = [], []
-        done = False
-
-        # Ensure the state has the correct shape
-        if not isinstance(state, np.ndarray) or state.shape != (len(test_env.state),):
-            state = np.reshape(state, (len(test_env.state),))  
-
-        while not done:
-            action_index, action  = agent.choose_action(state)
-            next_state, reward, done, _, _ = test_env.step(action)
-            val_episode_rewards.append(reward)
-            state = next_state
-            states.append(torch.from_numpy(state).float())
-           
-        total_val_rewards.append(sum(val_episode_rewards))
-
-        # Early stopping
-        if episode > 0 and len(total_val_rewards) > 1:
-            if total_val_rewards[-1] < total_val_rewards[-2]:
-                counter += 1
-                if counter == 3:
-                        break
-                else:
-                    counter = 0
-
-    # Calculate the rewards
-    avg_train_reward = sum(total_train_rewards) / episodes
-    avg_val_reward = sum(total_val_rewards) / episodes
-    print(f"Average Training Reward: {avg_train_reward}, Average Validation Reward: {avg_val_reward}")
-    
-    agent.save(model_save_path)
     return avg_val_reward
 
 if __name__ == "__main__":
